eec83575fb9c8cd07e23824ef49b84e602f0e116
Sary Jamie <sary.jamie@gmail.com>
2020-02-18 21:21:35 -0800

    updating files

--- db/seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
